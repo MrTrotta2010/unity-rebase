300c919dc2aa70d956947ba132705ec4aaf7cb1a
Tiago Trotta <mrtrotta2010@gmail.com>
2022-05-11 23:34:54 -0300

    Exceções de articulação retrabalhadas

---
+++ Models/RepeatedArticulationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class RepeatedArticulationException : Exception
+{
+    public string Patterns { get; }
+    public RepeatedArticulationException() { }
+
+    public RepeatedArticulationException(string message)
+        : base(message) { }
+
+    public RepeatedArticulationException(string message, Exception inner)
+        : base(message, inner) { }
+
+    public RepeatedArticulationException(string message, int[] articulations)
+    : this(message)
+    {
+        Patterns = $"[{string.Join(", ", articulations)}]";
+    }
+}
